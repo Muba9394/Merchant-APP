a04e0f3be312c7e8ac323e045fd4b70dff627953
MubarakKhan <Muba9394@users.noreply.github.com>
2017-11-11 12:59:36 +0530

    Commited

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 var krms_config ={			
-	'ApiUrl' : "https://www.cuisine.je/merchantapp/api",	
+	'ApiUrl' : "http://dev.cuisine.je/merchantapp/api",	
 	'DialogDefaultTitle' : "Cuisine",
 	'pushNotificationSenderid' : "306047836229",
 	'APIHasKey' : "db03847be0c7a3fb303f70f30520225c"
